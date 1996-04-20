e245c3454e6f9bef60b634c4f36651ee0dd17454
Fritz Elfert <fritz@wuemaus.franken.de>
1996-04-20 16:29:43 +0000

    Misc. typos

--- drivers/isdn/isdn_net.h
+++ drivers/isdn/isdn_net.h
@@ -1,6 +1,6 @@
 /* $Id$
  *
- * header for Linux ISDN subsystem, network releted funtions (linklevel).
+ * header for Linux ISDN subsystem, network related functions (linklevel).
  *
  * Copyright 1994,95,96 by Fritz Elfert (fritz@wuemaus.franken.de)
  * Copyright 1995,96    by Thinking Objects Software GmbH Wuerzburg
@@ -21,6 +21,9 @@
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. 
  *
  * $Log$
+ * Revision 1.1  1996/02/11 02:35:13  fritz
+ * Initial revision
+ *
  */
 
 extern char*         isdn_net_new(char *, struct device *);
