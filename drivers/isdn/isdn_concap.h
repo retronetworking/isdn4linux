86bd533dd20ded9aa1849c5d3bd25d330ebecf50
Karsten Keil <keil@temic-ech.spacenet.de>
1998-01-31 22:27:59 +0000

    New files from Henner Eisen for X.25 support

---
+++ drivers/isdn/isdn_concap.h
@@ -0,0 +1,6 @@
+/* $Id$
+extern struct concap_device_ops isdn_concap_reliable_dl_dops;
+extern struct concap_device_ops isdn_concap_demand_dial_dops;
+extern struct concap_proto * isdn_concap_new( int );
+
+
