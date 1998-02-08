77fd093a2f2a9b49402599ce0b1cd4944519f723
Karsten Keil <keil@temic-ech.spacenet.de>
1998-02-08 20:47:17 +0000

    Changes for 2.1 (virtual address mapping; SMP stuff) New CLI_DEBUG define to enable cli/sti/restore_flags debugging

--- drivers/isdn/kdebug.h
+++ drivers/isdn/kdebug.h
@@ -11,9 +11,13 @@
  *   are disabled. To work on virtual-screens with a screen-width other
  *   than 80, change the LINEWIDTH below.#
  *
+ *   For use with modules, you have to export screen_pos and fg_console
+ *   from console.c
+ *
  * Functionality:
  *
- *  -cli(), sti() and restore_flags() are redefined. Every time, cli()
+ *  -If you are defined CLI_DEBUG
+ *   cli(), sti() and restore_flags() are redefined. Every time, cli()
  *   is called, the filename and linenumber of the caller is printed
  *   on the console, preceeded by a "!". Once interrupts are enabled
  *   again, - either with sti() or restore_flags() - the exclamation
@@ -31,6 +35,9 @@
  *   position x,y.
  *
  * $Log$
+ * Revision 1.2  1997/02/03 23:33:22  fritz
+ * Reformatted according CodingStyle
+ *
  * Revision 1.1  1996/04/30 09:22:55  fritz
  * Taken under CVS-control.
  *
@@ -65,7 +72,7 @@ extern char clibuf[256];
 static __inline__ void
 put_ch(const char c)
 {
-	*scrpos++ = 0x0700 + c;
+	writew(0x0700 + c, scrpos++);
 }
 
 static __inline__ void
@@ -98,24 +105,47 @@ static __inline__ void
 wheel(int x, int y)
 {
 	gotovid(x, y);
-	switch ((char) (*scrpos & 0xff)) {
-		case '/':
-			*scrpos = 0x700 + '-';
-			break;
-		case '-':
-			*scrpos = 0x700 + '\\';
-			break;
-		case '\\':
-			*scrpos = 0x700 + '|';
-			break;
-		default:
-			*scrpos = 0x700 + '/';
+	switch ((char) (readw(scrpos) & 0xff)) {
+	case '/':
+		writew(0x0700 + '-', scrpos);
+		break;
+	case '-':
+		writew(0x0700 + '\\', scrpos);
+		break;
+	case '\\':
+		writew(0x0700 + '|', scrpos);
+		break;
+	default:
+		writew(0x0700 + '/', scrpos);
 	}
 }
 
+#ifdef CLI_DEBUG
+
+#ifndef __ASM_SYSTEM_H
+#include <asm/system.h>
+#endif
+
+#ifdef __SMP__
+
+extern void __global_cli(void);
+extern void __global_sti(void);
+extern void __global_restore_flags(unsigned long);
+#define x__cli() __global_cli()
+#define x__sti() __global_sti()
+#define x__restore_flags(x) __global_restore_flags(x)
+
+#else
+
+#define x__cli() __cli()
+#define x__sti() __sti()
+#define x__restore_flags(x) __restore_flags(x)
+
+#endif
+
 #undef cli
 #define cli() { \
-  __asm__ __volatile__ ("cli": : :"memory"); \
+  x__cli(); \
   sprintf(clibuf,"!%-20s %04d !",__BASE_FILE__,__LINE__); \
   gput_str(clibuf,0,0); \
 }
@@ -123,14 +153,15 @@ wheel(int x, int y)
 #undef restore_flags
 #define restore_flags(x) { \
   if (x & 0x200) gput_ch(' ',0,0); \
-  __asm__ __volatile__("pushl %0 ; popfl": /* no output */ :"r" (x):"memory"); \
+  x__restore_flags(x); \
 }
 
 #undef sti
 #define sti() { \
   gput_ch(' ',0,0); \
-  __asm__ __volatile__ ("sti": : :"memory"); \
+  x__sti(); \
 }
+#endif				/* CLI_DEBUG */
 
 #define CTR(x) { \
   gput_str("Enter " #x " ", 40, 0); \
