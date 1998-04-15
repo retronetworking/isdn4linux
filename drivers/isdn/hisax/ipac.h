40c56f63ab75598923887e89c395f1193323050f
Karsten Keil <keil@temic-ech.spacenet.de>
1998-04-15 16:48:09 +0000

    IPAC_ATX added

--- drivers/isdn/hisax/ipac.h
+++ drivers/isdn/hisax/ipac.h
@@ -6,6 +6,9 @@
  *
  *
  * $Log$
+ * Revision 1.2  1997/10/29 18:51:21  keil
+ * New files
+ *
  * Revision 1.1.2.1  1997/10/17 22:10:48  keil
  * new files on 2.0
  *
@@ -23,6 +26,7 @@
 #define IPAC_ACFG	0xC3
 #define IPAC_AOE	0xC4
 #define IPAC_ARX	0xC5
+#define IPAC_ATX	0xC5
 #define IPAC_PITA1	0xC6
 #define IPAC_PITA2	0xC7
 #define IPAC_POTA1	0xC8
