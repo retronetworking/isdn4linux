1fb7ca3ff49235ce9f98781f595cf99050e9dee8
Carsten Paeth <calle@calle.in-berlin.de>
1999-07-06 07:24:14 +0000

    Bugfix: call to kfree_skb in capidrv_signal was too early,         thanks to Lars Heete <hel@admin.de>.

--- drivers/isdn/avmb1/capidrv.c
+++ drivers/isdn/avmb1/capidrv.c
@@ -6,6 +6,22 @@
  * Copyright 1997 by Carsten Paeth (calle@calle.in-berlin.de)
  *
  * $Log$
+ * Revision 1.21  1999/07/01 15:26:34  calle
+ * complete new version (I love it):
+ * + new hardware independed "capi_driver" interface that will make it easy to:
+ *   - support other controllers with CAPI-2.0 (i.e. USB Controller)
+ *   - write a CAPI-2.0 for the passive cards
+ *   - support serial link CAPI-2.0 boxes.
+ * + wrote "capi_driver" for all supported cards.
+ * + "capi_driver" (supported cards) now have to be configured with
+ *   make menuconfig, in the past all supported cards where included
+ *   at once.
+ * + new and better informations in /proc/capi/
+ * + new ioctl to switch trace of capi messages per controller
+ *   using "avmcapictrl trace [contr] on|off|...."
+ * + complete testcircle with all supported cards and also the
+ *   PCMCIA cards (now patch for pcmcia-cs-3.0.13 needed) done.
+ *
  * Revision 1.20  1999/07/01 08:22:59  keil
  * compatibility macros now in <linux/isdn_compat.h>
  *
@@ -598,7 +614,7 @@ static void send_message(capidrv_contr * card, _cmsg * cmsg)
 	size_t len;
 	capi_cmsg2message(cmsg, cmsg->buf);
 	len = CAPIMSG_LEN(cmsg->buf);
-	skb = dev_alloc_skb(len);
+	skb = alloc_skb(len, GFP_ATOMIC);
 	memcpy(skb_put(skb, len), cmsg->buf, len);
 	(*capifuncs->capi_put_message) (global.appid, skb);
 	global.nsentctlpkt++;
@@ -1465,14 +1481,19 @@ static void capidrv_signal(__u16 applid, __u32 dummy)
 			global.nrecvdatapkt++;
 			continue;
 		}
-		kfree_skb(skb);
-		global.nrecvctlpkt++;
 		if ((s_cmsg.adr.adrController & 0xffffff00) == 0)
 			handle_controller(&s_cmsg);
 		else if ((s_cmsg.adr.adrPLCI & 0xffff0000) == 0)
 			handle_plci(&s_cmsg);
 		else
 			handle_ncci(&s_cmsg);
+		/*
+		 * data of skb used in s_cmsg,
+		 * free data when s_cmsg is not used again
+		 * thanks to Lars Heete <hel@admin.de>
+		 */
+		kfree_skb(skb);
+		global.nrecvctlpkt++;
 	}
 }
 
