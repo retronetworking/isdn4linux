974cd58b9ad2897d0e4a96853f1bf44098135930
Kai Germaschewski <kai@thphy.uni-duesseldorf.de>
2000-11-18 15:57:04 +0000

    compile fix if !CONFIG_PCI

--- drivers/isdn/hisax/nj_s.c
+++ drivers/isdn/hisax/nj_s.c
@@ -170,12 +170,11 @@ setup_netjet_s(struct IsdnCard *card))
 		return(0);
 	test_and_clear_bit(FLG_LOCK_ATOMIC, &cs->HW_Flags);
 
-	for ( ;; )
-	{
-
 #if CONFIG_PCI
 #ifdef COMPAT_HAS_NEW_PCI
 
+	for ( ;; )
+	{
 		if (!pci_present()) {
 			printk(KERN_ERR "Netjet: no PCI bus present\n");
 			return(0);
