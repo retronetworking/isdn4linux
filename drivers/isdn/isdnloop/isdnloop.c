6dc6a4b82219385df2072a9ec01577bcebea0f11
Kai Germaschewski <kai@thphy.uni-duesseldorf.de>
2001-11-11 19:54:31 +0000

    fix crash when compiled into the kernel

--- drivers/isdn/isdnloop/isdnloop.c
+++ drivers/isdn/isdnloop/isdnloop.c
@@ -1542,7 +1542,11 @@ isdnloop_init(void)
 	} else
 		strcpy(rev, " ??? ");
 	printk(KERN_NOTICE "isdnloop-ISDN-driver Rev%s\n", rev);
-	return (isdnloop_addcard(isdnloop_id));
+
+	if (isdnloop_id)
+		return (isdnloop_addcard(isdnloop_id));
+
+	return 0;
 }
 
 static void __exit
