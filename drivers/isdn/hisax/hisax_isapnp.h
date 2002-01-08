1703b79605760274879a3ebc1bb93b2fd7cc6491
Karsten Keil <keil@temic-ech.spacenet.de>
2002-01-08 17:20:34 +0000

    isapnp_driver is now part of <linux/isapnp.h>

--- drivers/isdn/hisax/hisax_isapnp.h
+++ drivers/isdn/hisax/hisax_isapnp.h
@@ -3,6 +3,7 @@
 
 #include <linux/isapnp.h>
 
+#ifdef COMPAT_NEED_ISAPNP_DRIVER
 struct isapnp_driver {
 	struct list_head node;
 	char *name;
@@ -10,7 +11,7 @@ struct isapnp_driver {
 	int  (*probe)  (struct pci_dev *dev, const struct isapnp_device_id *id);	/* New device inserted */
 	void (*remove) (struct pci_dev *dev);	/* Device removed (NULL if not a hot-plug capable driver) */
 };
-
+#endif
 #ifdef __ISAPNP__
 
 int isapnp_register_driver(struct isapnp_driver *drv);
