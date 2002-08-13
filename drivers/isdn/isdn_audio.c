0913c2ebe153058b201a1699cbb661b716002452
Karsten Keil <keil@temic-ech.spacenet.de>
2002-08-13 09:45:33 +0000

    change Christian Mock's address

--- drivers/isdn/isdn_audio.c
+++ drivers/isdn/isdn_audio.c
@@ -3,7 +3,7 @@
  * Linux ISDN subsystem, audio conversion and compression (linklevel).
  *
  * Copyright 1994-1999 by Fritz Elfert (fritz@isdn4linux.de)
- * DTMF code (c) 1996 by Christian Mock (cm@kukuruz.ping.at)
+ * DTMF code (c) 1996 by Christian Mock (cm@tahina.priv.at)
  * Silence detection (c) 1998 by Armin Schindler (mac@gismo.telekom.de)
  *
  * This software may be used and distributed according to the terms
