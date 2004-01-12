ab7211cbe7f4a1e1206a441f785a948ee3075a51
Karsten Keil <keil@temic-ech.spacenet.de>
2004-01-12 22:37:19 +0000

    first port from 2.4

--- drivers/isdn/i4l/isdn_bsdcomp.c
+++ drivers/isdn/i4l/isdn_bsdcomp.c
@@ -71,6 +71,7 @@
 
 #include <asm/system.h>
 #include <asm/bitops.h>
+#include <asm/segment.h>
 #include <asm/byteorder.h>
 #include <asm/types.h>
 
@@ -300,6 +301,7 @@ static void bsd_free (void *state)
 		 * Finally release the structure itself.
 		 */
 		kfree (db);
+		MOD_DEC_USE_COUNT;
 	}
 }
 
@@ -353,6 +355,8 @@ static void *bsd_alloc (struct isdn_ppp_comp_data *data)
 		bsd_free (db);
 		return NULL;
 	}
+
+	MOD_INC_USE_COUNT;
 
 	/*
 	 * If this is the compression buffer then there is no length data.
@@ -904,16 +908,16 @@ static int bsd_decompress (void *state, struct sk_buff *skb_in, struct sk_buff *
  *************************************************************/
 
 static struct isdn_ppp_compressor ippp_bsd_compress = {
-	.owner          = THIS_MODULE,
-	.num            = CI_BSD_COMPRESS,
-	.alloc          = bsd_alloc,
-	.free           = bsd_free,
-	.init           = bsd_init,
-	.reset          = bsd_reset,
-	.compress       = bsd_compress,
-	.decompress     = bsd_decompress,
-	.incomp         = bsd_incomp,
-	.stat           = bsd_stats,
+	NULL,NULL,		/* prev,next: overwritten by isdn_ppp */
+	CI_BSD_COMPRESS,	/* compress_proto */
+	bsd_alloc,		/* alloc */
+	bsd_free,		/* free */
+	bsd_init,		/* init */
+	bsd_reset,		/* reset */
+	bsd_compress,		/* compress */
+	bsd_decompress,		/* decompress */
+	bsd_incomp,		/* incomp */
+	bsd_stats		/* comp_stat */
 };
 
 /*************************************************************
