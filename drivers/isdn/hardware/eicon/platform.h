2bed1074f142b3c607313dc9e0f3d978113f5100
Karsten Keil <keil@temic-ech.spacenet.de>
2006-06-28 11:08:51 +0000

    rebase on mainstream patchset 2.6.8

--- drivers/isdn/hardware/eicon/platform.h
+++ drivers/isdn/hardware/eicon/platform.h
@@ -214,10 +214,7 @@ void diva_os_free_message_buffer(diva_os_message_buffer_s *dmb);
 */
 static __inline__ void diva_os_sleep(dword mSec)
 {
-	unsigned long timeout = HZ * mSec / 1000 + 1;
-
-	set_current_state(TASK_UNINTERRUPTIBLE);
-	schedule_timeout(timeout);
+	msleep(mSec);
 }
 static __inline__ void diva_os_wait(dword mSec)
 {
